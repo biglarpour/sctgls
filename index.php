5c7897bf6d0eb56d2af5b021c8bd9778928d03de
abocado <abo@adboomgrp.com>
2017-06-29 11:11:38 -0400

    Getting main look in for home page

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
-<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
-<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
-<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
-<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+<html class="no-js lt-ie9 lt-ie8 lt-ie7">
+<html class="no-js lt-ie9 lt-ie8">
+<html class="no-js lt-ie9">
+<html class="no-js">
 <head>
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
@@ -37,7 +37,7 @@
     <header class="wrapper clearfix">
 		       
         <div id="banner">        
-        	<div id="logo"><a href="basic.html"><img width="50%" src="images/logoidea1.jpg" alt="logo"></a></div>
+        	<div id="logo"><a href="/scoutinggoals"><img width="50%" src="images/logoidea1.jpg" alt="logo"></a></div>
         </div>
         
         <!-- main navigation -->
@@ -56,39 +56,20 @@
  
 <!-- hero area (the grey one with a slider -->
     <section id="hero" class="clearfix">    
-    <!-- responsive FlexSlider image slideshow -->
-    <div class="wrapper">
-       <div class="row"> 
-        <div class="grid_5">
-            <h1>Simple Responsive Template</h1>
-            <p>...is a template for responsive web design. A small set of tools and best practices that allows web designers to build responsive websites faster. Websites built with Simple Responsive Template will be optimized for screen widths between 320px and anything. Resize your browser to check it out.
-            </p>
-            <p><a href="#" class="buttonlink">CALL TO ACTION</a> <a href="#" class="buttonlink">ONE MORE</a></p>
-        </div>
-        <div class="grid_7 rightfloat">
-              <div class="flexslider">
-                  <ul class="slides">
-                      <li>
-                          <img src="images/basic-pic1.jpg" />
-                          <p class="flex-caption">Love Brazil !!! Sea view from Rio de Janeiro fort.</p>
-                      </li>
-                      <li>
-                          <a href="http://www.prowebdesign.ro"><img src="images/basic-pic2.jpg" /></a>
-                          <p class="flex-caption">El Arco Cabo Mexico. This image is wrapped in a link.</p>
-                      </li>
-                      <li>
-                          <img src="images/basic-pic3.jpg" />
-                          <p class="flex-caption">Arches National Park, Utah, Usa.</p>
-                      </li>
-                      <li>
-                          <img src="images/basic-pic4.jpg" />
-                          <p class="flex-caption">Morocco desert.</p>
-                      </li>
-                  </ul>
-                </div><!-- FlexSlider -->
-              </div><!-- end grid_7 -->
-        </div><!-- end row -->
-       </div><!-- end wrapper -->
+        <!-- responsive FlexSlider image slideshow -->
+        <div class="wrapper">
+           <div class="row">
+                <div class="grid_12">
+                      <div class="flexslider">
+                          <ul class="slides">
+                              <li>
+                                  <img src="images/night_camp.jpg" />
+                              </li>
+                          </ul>
+                      </div><!-- FlexSlider -->
+                </div><!-- end grid_7 -->
+            </div><!-- end row -->
+        </div><!-- end wrapper -->
     </section><!-- end hero area -->
 
 
@@ -97,27 +78,25 @@
 
 <!-- main content area -->   
 <div id="main" class="wrapper">
-    
+    <section id="quote" class="wide-content">
+        <div class="grid_4">
+            <h1>"I want the scouts to do the planning"</h1>
+            <h3>-Aaron Schultze,</h3><p>Scoutmaster</p>
+        </div>
+        <div class="grid_4">
+            <img src="images/logofav.png" />
+        </div>
+        <div class="grid_4">
+            <p>filler text about super cool scouting goals.  filler text about super cool scouting goals.  filler text about super cool scouting goals.  filler text about super cool scouting goals.  filler text about super cool scouting goals.  filler text about super cool scouting goals.  filler text about super cool scouting goals.  filler text about super cool scouting goals.
+            </p>
+        </div>
+    </section>
     
 <!-- content area -->    
 	<section id="content" class="wide-content">
       <div class="row">	
-        <div class="grid_4">
-        	<h1 class="first-header">Brazil!</h1>
-            <img src="images/basic-pic1.jpg" />
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
-        </div>
-        
-        <div class="grid_4">
-        	<h1 class="first-header">Mexico!</h1>
-            <img src="images/basic-pic2.jpg" />
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
-        </div>
-        
-        <div class="grid_4">
-        	<h1 class="first-header">US!</h1>
-            <img src="images/basic-pic3.jpg" />
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+        <div class="grid_12">
+            <img width="100%" src="images/home_forrest.jpg" />
         </div>
 	  </div><!-- end row -->
 	</section><!-- end content area -->   
